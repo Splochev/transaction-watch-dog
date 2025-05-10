a324e37b9be5e383ef88e4df06002f5e8cc1578c
Splochev <Stanislav.Plochev@gmail.com>
2025-05-10 22:40:59 +0300

    add zod validator

--- src/container/index.js
+++ src/container/index.js
@@ -3,15 +3,25 @@ const db = require("../db");
 const Logger = require("../logger/logger");
 const ErrorHandler = require("../utils/error-handler");
 const EthereumService = require("../services/ethereum-service");
+const ConfigurationService = require("../services/configuration-service");
+const EthereumValidator = require("../validators/ethereum");
+const {
+  transactionHashesSchema,
+  transactionsSchema,
+} = require("../schemas/ethereum");
 
 module.exports = function setupContainer() {
   const container = createContainer();
 
   container.register({
-    logger: asClass(Logger).singleton(),
     db: asValue(db),
+    logger: asClass(Logger).singleton(),
     errorHandler: asClass(ErrorHandler).singleton(),
     ethereumService: asClass(EthereumService).singleton(),
+    configurationService: asClass(ConfigurationService).singleton(),
+    ethereumValidator: asClass(EthereumValidator).singleton(),
+    transactionHashesSchema: asValue(transactionHashesSchema),
+    transactionsSchema: asValue(transactionsSchema),
   });
 
   return container;
---
+++ src/validators/ethereum.js
@@ -0,0 +1,39 @@
+class EthereumValidator {
+  constructor({ errorHandler, transactionHashesSchema, transactionsSchema }) {
+    if (EthereumValidator.instance) {
+      return EthereumValidator.instance;
+    }
+
+    this.transactionsSchema = transactionsSchema;
+    this.transactionHashesSchema = transactionHashesSchema;
+    this.errorHandler = errorHandler;
+
+    EthereumValidator.instance = this;
+  }
+
+  assertValidTransactionHashes(transactionHashes) {
+    try {
+      this.transactionHashesSchema.parse(transactionHashes);
+    } catch (error) {
+      throw this.errorHandler.generateError({
+        error,
+        message: "Invalid transaction hashes",
+        status: 400,
+      });
+    }
+  }
+
+  assertValidTransactionObjects(existingTransactions, checkForNull = true) {
+    if (!existingTransactions && !checkForNull) return;
+    try {
+      this.transactionsSchema.parse(existingTransactions);
+    } catch (error) {
+      throw this.errorHandler.generateError({
+        error,
+        message: "Invalid transaction objects",
+        status: 400,
+      });
+    }
+  }
+}
+module.exports = EthereumValidator;
